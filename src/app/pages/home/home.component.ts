1d44c8d558095e8061d7a32c1faa4c12be5c60d2
EDY1324 <prabowoedy80@gmail.com>
2025-02-11 16:41:05 +0700

    perbaruan lanjutan

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  hutaoSurprise() {
+    const sound = document.querySelector('audio');
+    sound?.play();
+    alert("Teng te teng! Kamu Kena Prank!");
+  }
 }
